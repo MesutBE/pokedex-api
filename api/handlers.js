d7ba7f1ba502a0ec6289f5d6bb9baae5574e6f18
mesut <gelukkig.zijn.be@gmail.com>
2020-05-31 09:26:55 +0200

    api solution

--- api/handlers.js
+++ api/handlers.js
@@ -11,11 +11,12 @@ const handlers = {
   valuesForKey: (req, res) => {
     // cast the user input to the correct type
     // according to the pokemon schema: ./data/schemas/pokemon.json
-    const key = Number(req.params.key);
+    const key = req.params.key;
+    const obj = JSON.parse(JSON.stringify(pokedex));
 
     // use the logic you wrote to process the pokedex data
     try {
-      const result = logic.valuesForKey(pokedex, key);
+      const result = logic.valuesForKey(obj.pokemon, key);
       console.log(result);
 
       // respond with the return value if there was no error
@@ -26,16 +27,16 @@ const handlers = {
       res.status(500).send(`${err.name}: ${err.message}`);
     }
   },
-  evolutionsOf: async (res, req) => { 
+  evolutionsOf: async (req, res) => {
 
     // cast the user input to the correct type
     // according to the pokemon schema: ./data/schemas/pokemon.json
     const name = req.params.name;
+    const obj = JSON.parse(JSON.stringify(pokedex));
 
     // use the logic you wrote to process the pokedex data
     try {
-      const result = logic.evolutionsOf(pokedex, name);
-      console.log(result);
+      const result = await logic.evolutionsOf(obj.pokemon, name);
 
       // respond with the return value if there was no error
       res.json(result);
@@ -45,12 +46,122 @@ const handlers = {
       res.status(500).send(`${err.name}: ${err.message}`);
     }
   },
-  typeStats: async (res, req) => { },
-  findById: async (res, req) => { },
-  findByType: async (res, req) => { },
-  findByWeakness: async (res, req) => { },
-  findByValue: async (res, req) => { },
-  findByKeyValue: async (res, req) => { },
+  typeStats: async (req, res) => {
+
+    // cast the user input to the correct type
+    // according to the pokemon schema: ./data/schemas/pokemon.json
+    const type = req.params.type;
+    const obj = JSON.parse(JSON.stringify(pokedex));
+
+
+    // use the logic you wrote to process the pokedex data
+    try {
+      const result = logic.typeStats(obj.pokemon, type);
+
+      // respond with the return value if there was no error
+      res.json(result);
+    } catch (err) {
+
+      // respond with 500 if there was an error
+      res.status(500).send(`${err.name}: ${err.message}`);
+    }
+  },
+  findById: async (req, res) => {
+
+    // cast the user input to the correct type
+    // according to the pokemon schema: ./data/schemas/pokemon.json
+    const id = Number(req.params.id);
+    const obj = JSON.parse(JSON.stringify(pokedex));
+
+    // use the logic you wrote to process the pokedex data
+    try {
+      const result = await logic.findById(obj.pokemon, id);
+
+      // respond with the return value if there was no error
+      res.json(result);
+    } catch (err) {
+
+      // respond with 500 if there was an error
+      res.status(500).send(`${err.name}: ${err.message}`);
+    }
+  },
+  findByType: async (req, res) => {
+
+    // cast the user input to the correct type
+    // according to the pokemon schema: ./data/schemas/pokemon.json
+    const type = req.params.type;
+    const obj = JSON.parse(JSON.stringify(pokedex));
+
+    // use the logic you wrote to process the pokedex data
+    try {
+      const result = await logic.findByType(obj.pokemon, type);
+
+      // respond with the return value if there was no error
+      res.json(result);
+    } catch (err) {
+
+      // respond with 500 if there was an error
+      res.status(500).send(`${err.name}: ${err.message}`);
+    }
+  },
+  findByWeakness: async (req, res) => {
+
+    // cast the user input to the correct type
+    // according to the pokemon schema: ./data/schemas/pokemon.json
+    const type = req.params.type;
+    const obj = JSON.parse(JSON.stringify(pokedex));
+
+    // use the logic you wrote to process the pokedex data
+    try {
+      const result = await logic.findByWeakness(obj.pokemon, type);
+
+      // respond with the return value if there was no error
+      res.json(result);
+    } catch (err) {
+
+      // respond with 500 if there was an error
+      res.status(500).send(`${err.name}: ${err.message}`);
+    }
+  },
+  findByValue: async (req, res) => {
+
+    // cast the user input to the correct type
+    // according to the pokemon schema: ./data/schemas/pokemon.json
+    const value = req.params.value;
+    const obj = JSON.parse(JSON.stringify(pokedex));
+
+    // use the logic you wrote to process the pokedex data
+    try {
+      const result = await logic.findByValue(obj.pokemon, value);
+
+      // respond with the return value if there was no error
+      res.json(result);
+    } catch (err) {
+
+      // respond with 500 if there was an error
+      res.status(500).send(`${err.name}: ${err.message}`);
+    }
+  },
+  findByKeyValue: async (req, res) => {
+
+    // cast the user input to the correct type
+    // according to the pokemon schema: ./data/schemas/pokemon.json
+    const key = req.params.key
+    const value = req.params.value;
+    const obj = JSON.parse(JSON.stringify(pokedex));
+
+    // use the logic you wrote to process the pokedex data
+    try {
+      const result = await logic.findByKeyValue(obj.pokemon, key, value);
+
+      // respond with the return value if there was no error
+      res.json(result);
+    } catch (err) {
+
+      // respond with 500 if there was an error
+      res.status(500).send(`${err.name}: ${err.message}`);
+    }
+  },
 }
 
 module.exports = handlers;
